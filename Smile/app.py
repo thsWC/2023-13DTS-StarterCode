b4e05baa2b7561a0d46a00c4ca9f75fd06e5d4c6
thsWC <99230647+thsWC@users.noreply.github.com>
2023-12-05 13:04:09 +1300

    Upload of files
    uploaded base files of the project.

---
+++ Smile/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_homepage():
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def render_menu_page():
+    return render_template('menu.html')
+
+
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
+
+
+app.run(host='0.0.0.0', debug=True)
